4c0587034295459d7655a5b131217fee18695a37
emoises <eduardo.moises@gmail.com>
2021-12-30 11:49:26 -0300

    Formating data

---
+++ utils.js
@@ -0,0 +1,19 @@
+const getDuration = (duration) => {
+  let durationValue = Number(duration.slice(0, duration.indexOf(" ") + 1));
+  let durationType = duration.slice(duration.indexOf(" ") + 1, duration.length);
+  if (durationType === "horas") {
+    return durationValue * 60 * 60 * 1000;
+  }
+  if (durationType === "minutos") {
+    return durationValue * 60 * 1000;
+  }
+  if (durationType === "segundos") {
+    return durationValue * 1000;
+  }
+
+  return null;
+};
+
+module.exports = {
+  getDuration,
+};
