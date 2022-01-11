7c0f9e981691b468c259d1327ae4b8465e2bd614
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-01-11 10:25:01 -0300

    Mudança de estilo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
+const { jobsApp } = require("./app");
+
+const maxDuration = 8 * 60 * 60 * 1000; //maxDuration is in miliseconds
+
 const fileBuffer = fs.readFileSync("./database/data.json");
 const { jobs } = JSON.parse(fileBuffer);
-const maxDuration = 8 * 60 * 60 * 1000; //maxDuration is in miliseconds
-const { jobsApp } = require("./app");
 
 const minDate = Date.parse("2019-11-10 09:00:00");
 const maxDate = Date.parse("2019-11-11 12:00:00");
